5333e59c3a0ce1e2c905c447bf0b3a64bb9681b8
Hackerma <clement.fang@hotmail.fr>
2017-08-01 00:25:36 +0200

    check all subdir

--- checkRendu.sh
+++ checkRendu.sh
@@ -5,12 +5,19 @@ GREEN='\033[0;32m'
 NC='\033[0m'
 
 clear
+array_o=`find . -name "*.o"`
+array_d=`find . -name "*.d"`
+array_swp=`find . -name ".*.swp"`
 
-[ ! -f *.o ] && echo ".o: ${GREEN}OK${NC}" || echo ".o: ${RED}KO${NC}"
+[ ${#array_o[@]} -gt 0 ] && echo ".o: ${RED}KO${NC}" || echo ".o: ${GREEN}OK${NC}"
 
-[ ! -f *.d ] && echo ".d: ${GREEN}OK${NC}" || echo ".d: ${RED}KO${NC}"
+[ ${#array_d[@]} -gt 0 ] && echo ".d: ${RED}KO${NC}" || echo ".d: ${GREEN}OK${NC}"
 
-[ ! -f *.swp ] && echo ".swp: ${GREEN}OK${NC}" || echo ".swp: ${RED}KO${NC}"
+[ ${#array_swp[@]} -gt 0 ] && echo ".swp: ${RED}KO${NC}" || echo ".swp: ${GREEN}OK${NC}"
+
+[ -f "TODO" ] && echo "TODO: ${GREEN}OK${NC}" || echo "TODO: ${RED}KO${NC}"
+
+[ -f "README" ] && echo "README: ${GREEN}OK${NC}" || echo "README: ${RED}KO${NC}"
 
 if [ -f "AUTHORS" ]; then
   length=$(wc -m < AUTHORS)
